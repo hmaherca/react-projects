3f1777ba48c7463e56f8caae85e10b999b2f63f7
unknown <hmaher.ca@gmail.com>
2018-03-06 19:26:19 -0500

    converted header to stateless

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-class Header extends React.Component{
-    render(){
-        console.log(this)
-        return(
-            <header className="top">
-                <h1>Catch 
-                <span className="ofThe">
-                    <span className="of">of</span>
-                    <span className="the">the</span>   
-                </span>
-                Day
-                </h1>
-                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-            </header>
-        )
-    }
+const Header = (props)=>{
+    return(
+        // this is a stateless functional component meaning that what it does will never change therefore we dont need 
+        // to make it a react component e.g extends . reat.component
+        // instead make a funtion that takes in props and returns the HTML we made.
+        <header className="top">
+            <h1>Catch 
+            <span className="ofThe">
+                <span className="of">of</span>
+                <span className="the">the</span>   
+            </span>
+            Day
+            </h1>
+            <h3 className="tagline"><span>{props.tagline}</span></h3>
+        </header>
+    )
+        
+
 }
+    
 
 export default Header
