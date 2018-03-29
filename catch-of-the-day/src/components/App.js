55ccbb3bc820bedaff97cc8602dee0413c1e04f8
unknown <hmaher.ca@gmail.com>
2018-03-29 13:53:16 -0400

    passed down addFish method

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,16 +6,27 @@ import Inventory from './Inventory'
 class App extends React.Component{
     constructor(){
         super();
+        this.addFish-this.addFish.bind(this);
+
         this.state={
             fishes: {},
             order:{}
         };
     }
-    
+
     addFish(fish){
         //update our state
-
+        // this... is an es6 way of copying (spread). it will take every item from our object and spread it into the new object we are declaring
+        const fishes={...this.state.fishes};
+        //add in our new fish
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
         //set state
+        this.setState(
+            {
+                fishes: fishes
+            }
+        )
     }
     render(){
         //'this' refers to the component, props is the object that contains the data we want
@@ -25,7 +36,7 @@ class App extends React.Component{
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
                 <Order/>
-                <Inventory/>
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
